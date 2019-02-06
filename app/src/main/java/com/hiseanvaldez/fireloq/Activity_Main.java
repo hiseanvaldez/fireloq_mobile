e976548e8fd98cdbcda0c53e753082d0873413c4
Shan Valdez <hisean.valdez@gmail.com>
2019-02-07 03:50:34 +0800

    Added Navigation Drawer

--- app/src/main/java/com/hiseanvaldez/fireloq/Activity_Main.java
+++ app/src/main/java/com/hiseanvaldez/fireloq/Activity_Main.java
@@ -1,22 +1,24 @@
 package com.hiseanvaldez.fireloq;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Activity_Main extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
     private FirebaseAuth mAuth;
     BottomNavigationView bottomNav;
+    Toolbar toolbar;
 
 
     @Override
@@ -25,6 +27,14 @@ public class Activity_Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         bottomNav = findViewById(R.id.nav_bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -61,4 +71,14 @@ public class Activity_Main extends AppCompatActivity {
             return true;
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
 }
